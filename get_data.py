433263a0a0019748335af6ec5acdcb758930ab73
Miłosz Zawolik <144321186+MiloszDev@users.noreply.github.com>
2026-09-23 19:32:31 +0200

    Refactor get_data.py by removing comments
    Removed comments and cleaned up the code.

--- get_data.py
+++ get_data.py
@@ -1,13 +1,10 @@
 import zipfile
 import os
 
-# Specify the path to the zip file and the directory to extract to
 zip_file_path = './data/stanford-dogs-dataset.zip'
 extract_to_path = './data'
 
-# Check if the zip file exists before attempting to extract
 if os.path.exists(zip_file_path):
-    # Open the zip file and extract its contents
     with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
         zip_ref.extractall(extract_to_path)
     
